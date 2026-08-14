f41e4ec6396929be096c7c1b1d4005dc1357eeb1
Mystic Maverick <rudrasankarg@gmail.com>
2026-08-15 01:54:01 +0530

    Add custom error handling middleware to backend

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -38,4 +38,16 @@ app.use("/api/v1/subscriptions", subscriptionRouter);
 app.use("/api/v1/playlist", playlistRouter);
 app.use("/api/v1/comments", commentRouter);
 
+// error handler middleware
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+    return res.status(statusCode).json({
+        statusCode,
+        success: false,
+        message,
+        errors: err.errors || []
+    });
+});
+
 export { app };
